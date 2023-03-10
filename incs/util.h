228485bada11c28dd34a8294845c87d4b35b52bd
junlee2 <junlee2@student.42seoul.kr>
2023-03-10 17:09:08 +0900

    Feat : ft_malloc - junlee2

--- incs/util.h
+++ incs/util.h
@@ -6,13 +6,16 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:48:17 by junlee2           #+#    #+#             */
-/*   Updated: 2023/03/10 15:49:00 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2023/03/10 17:06:19 by junlee2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTIL_H
 # define UTIL_H
 
+# include <stddef.h>
+
 void	err_exit(char *str);
+void	*ft_malloc(size_t size);
 
 #endif
--- srcs/util.c
+++ srcs/util.c
@@ -6,10 +6,11 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 15:41:04 by junlee2           #+#    #+#             */
-/*   Updated: 2023/03/10 16:44:02 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2023/03/10 17:05:25 by junlee2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -17,4 +18,14 @@ void	err_exit(char *str)
 {
 	perror(str);
 	exit(EXIT_FAILURE);
+}
+
+void	*ft_malloc(size_t size)
+{
+	void	*temp;
+
+	temp = malloc(size);
+	if (!temp)
+		err_exit("malloc err");
+	return (temp);
 }
