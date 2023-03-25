03691aff0b3eb24039804ed6dbfde277aecbf834
sounchoi <ragi230@naver.com>
2023-03-25 21:10:51 +0900

    Docs : add contents input_data.c - sounchoi

--- srcs/input_data/input_data.c
+++ srcs/input_data/input_data.c
@@ -10,3 +10,35 @@ void	input_player(t_map_obj *map_obj, t_player *player)
 	player->pos_x = map_obj->player_pos[0];
 	player->pos_y = map_obj->player_pos[1];
 }
+
+void	dir_plane_input(int x, t_player *player)
+{
+	if (x == 0)
+	{
+		player->dir_x = -1;
+		player->dir_y = 0;
+		player->plane_x = 0;
+		player->plane_y = 0.66;
+	}
+	else if (x == 1)
+	{
+		player->dir_x = 1;
+		player->dir_y = 0;
+		player->plane_x = 0;
+		player->plane_y = -0.66;
+	}
+	else if (x == 2)
+	{
+		player->dir_x = 0.0;
+		player->dir_y = -1.0;
+		player->plane_x = 0.66;
+		player->plane_y = 0.0;
+	}
+	else if (x == 3)
+	{
+		player->dir_x = 0.0;
+		player->dir_y = 1.0;
+		player->plane_x = -0.66;
+		player->plane_y = 0.0;
+	}
+}
