36a8c9456b17e2e8b9654eaef1ec69e6ad60d5e2
junlee2 <junlee2@student.42seoul.kr>
2023-05-26 17:23:04 +0900

    2023/05/26 - 17:23:04 backup

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+./cub3D Map/Error0.cubb
+./cub3D Map/Error1.cub
+./cub3D Map/Error2.cub
+./cub3D Map/Error3.cub
+./cub3D Map/Error4.cub
+./cub3D Map/Error5.cub
+./cub3D Map/Error6.cub
+./cub3D Map/Error7.cub
+./cub3D Map/Error8.cub
+./cub3D Map/Error9.cub
+./cub3D Map/Error10.cub
+./cub3D Map/Error11.cub
+./cub3D Map/Error12.cub
+./cub3D Map/Error13.cub
+./cub3D Map/Error14.cub
+./cub3D Map/Error15.cub
